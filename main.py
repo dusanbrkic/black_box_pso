dcb588ce77e98f5456b2992efc032d63bc897e26
Filip <filip.zivanac@gmail.com>
2020-01-14 20:33:08 +0100

    a rate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pyswarm import pso
 import numpy as np
 import json
+from pso import particle_swarm_optimisation
 
 
 
@@ -9,13 +10,4 @@ from ann_criterion import optimality_criterion
 
 
 if __name__ == "__main__":
-    w = list(np.random.uniform(-10, 10, 60))
-    lb = [-10]*60
-    up = [10]*60
-
-    xopt, fopt = pso(optimality_criterion, lb, up, maxiter=20)
-    f = open("rezultati.txt", "a")
-    f.write(str(xopt)+"\n"+str(fopt)+"\n")
-    f.close()
-    print(xopt)
-    print(fopt)
+    X = pso()
---
+++ pso.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+class Paricle():
+    def __init__(self, lb, ub, dim):
+        self.pozicija = np.random.uniform(low=lb, high=ub, size=dim)
+        self.vektor = np.full(dim, 0, dtype=float)
+        self.y = np.Inf
+        self.najbolji_X = self.pozicija
+        self.najbolji_y = self.y
+
+
+
+
+
+def particle_swarm_optimisation(funkcija, lowerb, upperb, maxiter=100, npart = 50, dim=60):
+
+    w = 1
+    dmp = 0.99
+    c1 = 2
+    c2 = 2
+    #Inicijalizacija
+    particles = []
+    globalno_najbolji_X = "Neka najbolji pobedi"
+    globalno_najbolji_y = np.Inf
+    for k in range(dim):
+        p = Paricle(lowerb, upperb, dim)
+        p.y = funkcija(p.pozicija)
+        particles.append(p)
+        if p.y<globalno_najbolji_y:
+            globalno_najbolji_X = p.pozicija
+            globalno_najbolji_y = p.y
+
+    #glavna petlja
+
+    for i in range(maxiter):
+        for j in range(len(particles)):
+            particles[j].vektor = w*particles[j].vektor + c1*np.random.uniform(size=dim)*(particles[j].najbolji_X - particles[j].pozicija)+c2*np.random.uniform(size=dim)*(globalno_najbolji_X-particles[j].pozicija)
+
+            particles[j].pozicija = particles[j] + particles[j].vektor
+
+            particles[j].y = funkcija(particles[j].pozicija)
+
+            if particles[j].y < particles[j].najbolji_y:
+                particles[j].najbolji_X = particles[j].pozicija
+                particles[j].najbolji_y = particles[j].y
+                if particles[j].y < globalno_najbolji_y:
+                    globalno_najbolji_X = particles[j].pozicija
+                    globalno_najbolji_y = particles[j].y
+            w = w*dmp
+    return
